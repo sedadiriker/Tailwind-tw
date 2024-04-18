2772677ae176d72f5c2782268a882aeb514be8d4
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-18 10:47:49 +0300

    navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Solutions', href: '#', current: false },
+  { name: 'Industries', href: '#', current: false },
+  { name: 'Fees', href: '#', current: false },
+  { name: 'About Rareblocks', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" >
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-full px-5">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-start md:items-stretch md:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                
+                  <img
+                    className="h-8 w-auto"
+                    src="https://d33wubrfki0l68.cloudfront.net/682a555ec15382f2c6e7457ca1ef48d8dbb179ac/f8cd3/images/logo.svg" 
+                    alt="logo"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 md:block">
+                  <div className="flex space-x-4 md:space-x-2">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-black-500 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm md:text-xs font-medium whitespace-nowrap'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:block absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 gap-1">
+                <a href="#" target="_blank" rel="noopener noreferrer" className=" text-black-500 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm md:text-xs font-medium "> Sign in</a>
+
+                <a href="#" target="_blank" className="bg-gray-900 text-white rounded-md px-3 py-2 text-xs md:text-[12px] font-medium" >Create free account</a>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="md:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
